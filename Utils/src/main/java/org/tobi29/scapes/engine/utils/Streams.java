56cdf37b231096430f7a12916ae573a40404c327
Tobi29 <tobi291019@gmail.com>
2016-03-04 06:10:22 +0100

    Allow specifying stream type if necessary

--- Utils/src/main/java/org/tobi29/scapes/engine/utils/Streams.java
+++ Utils/src/main/java/org/tobi29/scapes/engine/utils/Streams.java
@@ -10,22 +10,51 @@ import java.util.Collection;
 
 public class Streams {
     public static <T> Stream<T> of(Collection<T> collection) {
-        return StreamSupport.stream(collection);
+        return ofCollection(collection);
     }
 
     public static <T> Stream<T> of(Pool<T> pool) {
-        return of(pool.spliterator8());
+        return ofPool(pool);
     }
 
     public static <T> Stream<T> of() {
-        return RefStreams.empty();
+        return ofEmpty();
     }
 
     public static <T> Stream<T> of(T item) {
-        return RefStreams.of(item);
+        return ofObject(item);
     }
 
     public static <T> Stream<T> of(Optional<T> item) {
+        return ofOptional(item);
+    }
+
+    @SafeVarargs
+    public static <T> Stream<T> of(T... array) {
+        return ofArray(array);
+    }
+
+    public static <T> Stream<T> of(Spliterator<T> spliterator) {
+        return ofSpliterator(spliterator);
+    }
+
+    public static <T> Stream<T> ofCollection(Collection<T> collection) {
+        return StreamSupport.stream(collection);
+    }
+
+    public static <T> Stream<T> ofPool(Pool<T> pool) {
+        return of(pool.spliterator8());
+    }
+
+    public static <T> Stream<T> ofEmpty() {
+        return RefStreams.empty();
+    }
+
+    public static <T> Stream<T> ofObject(T item) {
+        return RefStreams.of(item);
+    }
+
+    public static <T> Stream<T> ofOptional(Optional<T> item) {
         if (item.isPresent()) {
             return RefStreams.of(item.get());
         }
@@ -33,11 +62,11 @@ public class Streams {
     }
 
     @SafeVarargs
-    public static <T> Stream<T> of(T... array) {
+    public static <T> Stream<T> ofArray(T... array) {
         return RefStreams.of(array);
     }
 
-    public static <T> Stream<T> of(Spliterator<T> spliterator) {
+    public static <T> Stream<T> ofSpliterator(Spliterator<T> spliterator) {
         return StreamSupport.stream(spliterator, false);
     }
 }
