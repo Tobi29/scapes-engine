5727a8679377ea6a8ceab78c02fee5d618758bd9
Tobi29 <tobi291019@gmail.com>
2016-05-13 00:24:29 +0200

    Store size in vector

--- src/main/java/org/tobi29/scapes/engine/gui/GuiLayoutData.java
+++ src/main/java/org/tobi29/scapes/engine/gui/GuiLayoutData.java
@@ -1,37 +1,42 @@
 package org.tobi29.scapes.engine.gui;
 
 import java8.util.Optional;
+import org.tobi29.scapes.engine.utils.math.vector.MutableVector2;
+import org.tobi29.scapes.engine.utils.math.vector.MutableVector2d;
 import org.tobi29.scapes.engine.utils.math.vector.Vector2;
 
 public class GuiLayoutData {
     private final Optional<GuiComponent> parent;
     private final long priority;
     private final boolean blocksEvents;
-    private double width, height;
+    private final MutableVector2 size = new MutableVector2d();
 
     public GuiLayoutData(Optional<GuiComponent> parent, Vector2 size,
             long priority, boolean blocksEvents) {
         this.parent = parent;
-        width = size.doubleX();
-        height = size.doubleY();
+        this.size.set(size);
         this.priority = priority;
         this.blocksEvents = blocksEvents;
     }
 
     public double width() {
-        return width;
+        return size.doubleX();
     }
 
     public double height() {
-        return height;
+        return size.doubleY();
     }
 
     public void setWidth(double value) {
-        width = value;
+        size.setX(value);
     }
 
     public void setHeight(double value) {
-        height = value;
+        size.setY(value);
+    }
+
+    public void setSize(Vector2 size) {
+        this.size.set(size);
     }
 
     public Optional<GuiComponent> parent() {
