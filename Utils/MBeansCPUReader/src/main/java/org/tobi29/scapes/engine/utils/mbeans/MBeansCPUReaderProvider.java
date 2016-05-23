addf9f04a760e5a2eb1319cf87e59015003ef8d1
Tobi29 <tobi291019@gmail.com>
2016-05-23 04:07:26 +0200

    Fix garbage values from cpu reader

--- Utils/MBeansCPUReader/src/main/java/org/tobi29/scapes/engine/utils/mbeans/MBeansCPUReaderProvider.java
+++ Utils/MBeansCPUReader/src/main/java/org/tobi29/scapes/engine/utils/mbeans/MBeansCPUReaderProvider.java
@@ -1,14 +1,12 @@
 package org.tobi29.scapes.engine.utils.mbeans;
 
-import java8.util.concurrent.ConcurrentMaps;
+import java8.util.Maps;
 import org.tobi29.scapes.engine.utils.CPUUtil;
 import org.tobi29.scapes.engine.utils.spi.CPUReaderProvider;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.*;
 
 public class MBeansCPUReaderProvider implements CPUReaderProvider {
     private static final ThreadMXBean THREADS =
@@ -22,8 +20,8 @@ public class MBeansCPUReaderProvider implements CPUReaderProvider {
     @Override
     public CPUUtil.Reader reader() {
         return new CPUUtil.Reader() {
-            private final ConcurrentMap<Long, Long> lastThreadTimes =
-                    new ConcurrentHashMap<>();
+            private final Map<Long, Long> threadTimes = new HashMap<>();
+            private final Set<Long> threads = new HashSet<>();
             private long lastTime;
 
             @Override
@@ -40,20 +38,22 @@ public class MBeansCPUReaderProvider implements CPUReaderProvider {
                     }
                     long time = System.nanoTime();
                     double delta = time - lastTime;
-                    Map<Long, Long> threadTimes;
-                    if (threads.length >= lastThreadTimes.size()) {
-                        threadTimes = lastThreadTimes;
-                    } else {
-                        threadTimes = new ConcurrentHashMap<>();
-                    }
                     for (long thread : threads) {
                         long threadTime = THREADS.getThreadCpuTime(thread);
-                        double threadDelta = threadTime - ConcurrentMaps
-                                .getOrDefault(lastThreadTimes, thread, 0L);
+                        double threadDelta = threadTime -
+                                Maps.getOrDefault(threadTimes, thread, 0L);
+                        this.threads.add(thread);
                         threadTimes.put(thread, threadTime);
                         threadDelta /= delta;
                         cpu += threadDelta;
                     }
+                    Iterator<Long> iterator = threadTimes.keySet().iterator();
+                    while (iterator.hasNext()) {
+                        if (!this.threads.contains(iterator.next())) {
+                            iterator.remove();
+                        }
+                    }
+                    this.threads.clear();
                     lastTime = time;
                 } else {
                     cpu = Double.NaN;
