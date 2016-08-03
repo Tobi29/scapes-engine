278e2de3bfb3ce6a9258a8607ae4d83d8be6b691
Tobi29 <tobi291019@gmail.com>
2016-08-03 05:16:48 +0200

    Update copyright

--- Codecs/CodecWAV/src/main/java/org/tobi29/scapes/engine/utils/codec/wav/WAVReadStream.java
+++ Codecs/CodecWAV/src/main/java/org/tobi29/scapes/engine/utils/codec/wav/WAVReadStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 Tobi29
+ * Copyright 2012-2016 Tobi29
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 // Based on: http://www.labbookpages.co.uk/audio/wavFiles.html
 package org.tobi29.scapes.engine.utils.codec.wav;
 
--- src/main/java/org/tobi29/scapes/engine/graphics/FontRenderer.java
+++ src/main/java/org/tobi29/scapes/engine/graphics/FontRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 Tobi29
+ * Copyright 2012-2016 Tobi29
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.tobi29.scapes.engine.graphics;
 
 import java8.util.Maps;
