85faea054f8ed31ebe315aada317c1e0d69a0105
Tobi29 <tobi291019@gmail.com>
2017-04-19 04:57:59 +0200

    Fix name resolver not unwrapping companion objects

--- Utils/src/jvm/kotlin/org/tobi29/scapes/engine/utils/logging/internal/KLoggerNameResolverJVM.kt
+++ Utils/src/jvm/kotlin/org/tobi29/scapes/engine/utils/logging/internal/KLoggerNameResolverJVM.kt
@@ -7,15 +7,15 @@ import kotlin.reflect.KClass
 @Suppress("NOTHING_TO_INLINE")
 internal object KLoggerNameResolver {
     inline internal fun <T : Any> name(forClass: KClass<T>): String =
-            KLoggerNameResolver.unwrapCompanionClass(
-                    forClass).java.simpleName ?: "???"
+            unwrapCompanionClass(forClass).java.simpleName ?: "???"
 
     inline private fun <T : Any> unwrapCompanionClass(clazz: KClass<T>): KClass<*> {
         if (clazz.java.enclosingClass != null) {
             try {
                 val field = clazz.java.enclosingClass.getField(
                         clazz.java.simpleName)
-                if (Modifier.isStatic(field.modifiers) && field.type == clazz) {
+                if (Modifier.isStatic(field.modifiers)
+                        && field.type == clazz.java) {
                     return clazz.java.enclosingClass.kotlin
                 }
             } catch(e: Exception) {
