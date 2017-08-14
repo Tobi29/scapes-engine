c8bbaab3be32058bc8eda179b4935f45b4fc1f5d
Tobi29 <tobi291019@gmail.com>
2017-08-14 12:00:09 +0200

    Use external repository for kotlinx coroutines

--- KotlinxCoroutinesCore/src/main/kotlin/java/lang/Runnable.kt
+++
@@ -1,6 +0,0 @@
-package java.lang
-
-@Suppress("HEADER_WITHOUT_IMPLEMENTATION")
-header interface Runnable {
-    fun run()
-}
--- KotlinxCoroutinesCore/src/main/kotlin/java/util/concurrent/CancellationException.kt
+++
@@ -1,8 +0,0 @@
-package java.util.concurrent
-
-@Suppress("HEADER_WITHOUT_IMPLEMENTATION")
-header open class CancellationException : IllegalStateException {
-    constructor()
-
-    constructor(message: String)
-}
