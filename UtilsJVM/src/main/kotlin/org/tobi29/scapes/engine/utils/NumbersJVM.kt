2bff59d11cb40cc447c9a411c6250babbcc30363
Tobi29 <tobi291019@gmail.com>
2017-09-26 00:33:52 +0200

    Use raw floating point bits

--- UtilsJVM/src/main/kotlin/org/tobi29/scapes/engine/utils/NumbersJVM.kt
+++ UtilsJVM/src/main/kotlin/org/tobi29/scapes/engine/utils/NumbersJVM.kt
@@ -3,10 +3,10 @@
 package org.tobi29.scapes.engine.utils
 
 impl inline fun Float.bits(): Int =
-        java.lang.Float.floatToIntBits(this)
+        java.lang.Float.floatToRawIntBits(this)
 
 impl inline fun Double.bits(): Long =
-        java.lang.Double.doubleToLongBits(this)
+        java.lang.Double.doubleToRawLongBits(this)
 
 impl inline fun Int.bitsToFloat(): Float =
         java.lang.Float.intBitsToFloat(this)
