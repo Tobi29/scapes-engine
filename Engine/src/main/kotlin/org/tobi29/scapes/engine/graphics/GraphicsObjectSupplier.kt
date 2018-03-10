ed50735acdeac189a9c3c8376ff8e0102d7c6b19
Git out :V <tobi291019@gmail.com>
2018-03-10 02:57:22 +0100

    Allow loading aot compiled shader

--- Engine/src/main/kotlin/org/tobi29/scapes/engine/graphics/GraphicsObjectSupplier.kt
+++ Engine/src/main/kotlin/org/tobi29/scapes/engine/graphics/GraphicsObjectSupplier.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 Tobi29
+ * Copyright 2012-2018 Tobi29
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,17 @@
 
 package org.tobi29.scapes.engine.graphics
 
-import org.tobi29.scapes.engine.Container
 import org.tobi29.graphics.Image
 import org.tobi29.io.ByteViewRO
+import org.tobi29.io.IOException
+import org.tobi29.io.ReadSource
+import org.tobi29.io.tag.binary.readBinary
 import org.tobi29.io.view
+import org.tobi29.scapes.engine.Container
+import org.tobi29.scapes.engine.resource.Resource
 import org.tobi29.scapes.engine.shader.CompiledShader
 import org.tobi29.scapes.engine.shader.Expression
+import org.tobi29.scapes.engine.shader.toCompiledShader
 
 interface GraphicsObjectSupplier {
     val container: Container
@@ -30,98 +35,140 @@ interface GraphicsObjectSupplier {
     val fboTracker: GraphicsObjectTracker<Framebuffer>
     val shaderTracker: GraphicsObjectTracker<Shader>
 
-    fun createTexture(width: Int,
-                      height: Int): Texture {
-        return createTexture(width, height, ByteArray(width * height * 4).view,
-                0)
-    }
+    fun createTexture(
+        width: Int,
+        height: Int
+    ): Texture =
+        createTexture(width, height, ByteArray(width * height * 4).view, 0)
 
-    fun createTexture(image: Image,
-                      mipmaps: Int): Texture {
-        return createTexture(image.width, image.height, image.view, mipmaps,
-                TextureFilter.NEAREST, TextureFilter.NEAREST,
-                TextureWrap.REPEAT, TextureWrap.REPEAT)
-    }
+    fun createTexture(
+        image: Image,
+        mipmaps: Int
+    ): Texture =
+        createTexture(
+            image.width, image.height, image.view, mipmaps,
+            TextureFilter.NEAREST, TextureFilter.NEAREST,
+            TextureWrap.REPEAT, TextureWrap.REPEAT
+        )
 
-    fun createTexture(width: Int,
-                      height: Int,
-                      mipmaps: Int): Texture {
-        return createTexture(width, height, ByteArray(width * height * 4).view,
-                mipmaps)
-    }
+    fun createTexture(
+        width: Int,
+        height: Int,
+        mipmaps: Int
+    ): Texture =
+        createTexture(
+            width, height, ByteArray(width * height * 4).view, mipmaps
+        )
 
-    fun createTexture(width: Int,
-                      height: Int,
-                      mipmaps: Int,
-                      minFilter: TextureFilter,
-                      magFilter: TextureFilter,
-                      wrapS: TextureWrap,
-                      wrapT: TextureWrap): Texture {
-        return createTexture(width, height, ByteArray(width * height * 4).view,
-                mipmaps, minFilter, magFilter, wrapS, wrapT)
-    }
+    fun createTexture(
+        width: Int,
+        height: Int,
+        mipmaps: Int,
+        minFilter: TextureFilter,
+        magFilter: TextureFilter,
+        wrapS: TextureWrap,
+        wrapT: TextureWrap
+    ): Texture =
+        createTexture(
+            width, height, ByteArray(width * height * 4).view, mipmaps,
+            minFilter, magFilter, wrapS, wrapT
+        )
 
-    fun createTexture(image: Image,
-                      mipmaps: Int = 0,
-                      minFilter: TextureFilter = TextureFilter.NEAREST,
-                      magFilter: TextureFilter = TextureFilter.NEAREST,
-                      wrapS: TextureWrap = TextureWrap.REPEAT,
-                      wrapT: TextureWrap = TextureWrap.REPEAT): Texture {
-        return createTexture(image.width, image.height, image.view, mipmaps,
-                minFilter, magFilter, wrapS, wrapT)
-    }
+    fun createTexture(
+        image: Image,
+        mipmaps: Int = 0,
+        minFilter: TextureFilter = TextureFilter.NEAREST,
+        magFilter: TextureFilter = TextureFilter.NEAREST,
+        wrapS: TextureWrap = TextureWrap.REPEAT,
+        wrapT: TextureWrap = TextureWrap.REPEAT
+    ): Texture =
+        createTexture(
+            image.width, image.height, image.view, mipmaps,
+            minFilter, magFilter, wrapS, wrapT
+        )
 
-    fun createTexture(width: Int,
-                      height: Int,
-                      buffer: ByteViewRO,
-                      mipmaps: Int = 0,
-                      minFilter: TextureFilter = TextureFilter.NEAREST,
-                      magFilter: TextureFilter = TextureFilter.NEAREST,
-                      wrapS: TextureWrap = TextureWrap.REPEAT,
-                      wrapT: TextureWrap = TextureWrap.REPEAT): Texture
+    fun createTexture(
+        width: Int,
+        height: Int,
+        buffer: ByteViewRO,
+        mipmaps: Int = 0,
+        minFilter: TextureFilter = TextureFilter.NEAREST,
+        magFilter: TextureFilter = TextureFilter.NEAREST,
+        wrapS: TextureWrap = TextureWrap.REPEAT,
+        wrapT: TextureWrap = TextureWrap.REPEAT
+    ): Texture
 
-    fun createFramebuffer(width: Int,
-                          height: Int,
-                          colorAttachments: Int,
-                          depth: Boolean,
-                          hdr: Boolean,
-                          alpha: Boolean,
-                          minFilter: TextureFilter = TextureFilter.NEAREST,
-                          magFilter: TextureFilter = minFilter): Framebuffer
+    fun createFramebuffer(
+        width: Int,
+        height: Int,
+        colorAttachments: Int,
+        depth: Boolean,
+        hdr: Boolean,
+        alpha: Boolean,
+        minFilter: TextureFilter = TextureFilter.NEAREST,
+        magFilter: TextureFilter = minFilter
+    ): Framebuffer
 
-    fun createModelFast(attributes: List<ModelAttribute>,
-                        length: Int,
-                        renderType: RenderType): Model
+    fun createModelFast(
+        attributes: List<ModelAttribute>,
+        length: Int,
+        renderType: RenderType
+    ): Model
 
-    fun createModelStatic(attributes: List<ModelAttribute>,
-                          length: Int,
-                          index: IntArray,
-                          renderType: RenderType): Model {
-        return createModelStatic(attributes, length, index, index.size,
-                renderType)
-    }
+    fun createModelStatic(
+        attributes: List<ModelAttribute>,
+        length: Int,
+        index: IntArray,
+        renderType: RenderType
+    ): Model =
+        createModelStatic(attributes, length, index, index.size, renderType)
 
-    fun createModelStatic(attributes: List<ModelAttribute>,
-                          length: Int,
-                          index: IntArray,
-                          indexLength: Int,
-                          renderType: RenderType): Model
+    fun createModelStatic(
+        attributes: List<ModelAttribute>,
+        length: Int,
+        index: IntArray,
+        indexLength: Int,
+        renderType: RenderType
+    ): Model
 
     fun createModelHybrid(
-            attributes: List<ModelAttribute>,
-            length: Int,
-            attributesStream: List<ModelAttribute>,
-            lengthStream: Int,
-            renderType: RenderType): ModelHybrid
+        attributes: List<ModelAttribute>,
+        length: Int,
+        attributesStream: List<ModelAttribute>,
+        lengthStream: Int,
+        renderType: RenderType
+    ): ModelHybrid
 
-    fun createShader(shader: CompiledShader,
-                     properties: Map<String, Expression> = emptyMap()): Shader
+    fun createShader(
+        shader: CompiledShader,
+        properties: Map<String, Expression> = emptyMap()
+    ): Shader
 }
 
-fun GraphicsSystem.loadShader(shader: suspend () -> CompiledShader,
-                              properties: Map<String, Expression> = emptyMap()) =
-        engine.resources.load { createShader(shader(), properties) }
 
-fun GraphicsSystem.loadShader(shader: CompiledShader,
-                              properties: Map<String, Expression> = emptyMap()) =
-        engine.resources.load { createShader(shader, properties) }
+fun GraphicsSystem.loadShader(
+    asset: String,
+    properties: Map<String, Expression> = emptyMap()
+): Resource<Shader> =
+    loadShader(engine.files[asset], properties)
+
+fun GraphicsSystem.loadShader(
+    asset: ReadSource,
+    properties: Map<String, Expression> = emptyMap()
+): Resource<Shader> =
+    loadShader({
+        asset.readAsync { readBinary(it) }.toCompiledShader()
+                ?: throw IOException("Failed to deserialize shader")
+    }, properties)
+
+fun GraphicsSystem.loadShader(
+    shader: suspend () -> CompiledShader,
+    properties: Map<String, Expression> = emptyMap()
+): Resource<Shader> =
+    engine.resources.load { createShader(shader(), properties) }
+
+fun GraphicsSystem.loadShader(
+    shader: CompiledShader,
+    properties: Map<String, Expression> = emptyMap()
+): Resource<Shader> =
+    engine.resources.load { createShader(shader, properties) }
