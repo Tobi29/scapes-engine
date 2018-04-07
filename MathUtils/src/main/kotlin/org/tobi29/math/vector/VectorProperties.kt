7bf5d83c221d6b109dbe898c78f22e1f01da67bd
Git out :V <tobi291019@gmail.com>
2018-04-08 01:13:00 +0200

    Add delegated property support for mutable vectors

---
+++ MathUtils/src/main/kotlin/org/tobi29/math/vector/VectorProperties.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012-2018 Tobi29
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("NOTHING_TO_INLINE")
+
+package org.tobi29.math.vector
+
+import kotlin.reflect.KProperty
+
+inline operator fun MutableVector2d.getValue(
+    thisRef: Any?, property: KProperty<*>
+): Vector2d = now()
+
+inline operator fun MutableVector2d.setValue(
+    thisRef: Any?, property: KProperty<*>, value: ReadVector2d
+) {
+    set(value)
+}
+
+inline operator fun MutableVector3d.getValue(
+    thisRef: Any?, property: KProperty<*>
+): Vector3d = now()
+
+inline operator fun MutableVector3d.setValue(
+    thisRef: Any?, property: KProperty<*>, value: ReadVector3d
+) {
+    set(value)
+}
+
+inline operator fun MutableVector2i.getValue(
+    thisRef: Any?, property: KProperty<*>
+): Vector2i = now()
+
+inline operator fun MutableVector2i.setValue(
+    thisRef: Any?, property: KProperty<*>, value: ReadVector2i
+) {
+    set(value)
+}
+
+inline operator fun MutableVector3i.getValue(
+    thisRef: Any?, property: KProperty<*>
+): Vector3i = now()
+
+inline operator fun MutableVector3i.setValue(
+    thisRef: Any?, property: KProperty<*>, value: ReadVector3i
+) {
+    set(value)
+}
