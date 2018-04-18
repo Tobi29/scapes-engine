502c0fa4fd8e133bb31fe7389caa2c2d854c2202
Git out :V <tobi291019@gmail.com>
2018-04-18 21:48:52 +0200

    Use own version of annotation

--- STDEx/src/main/kotlin/org/tobi29/stdex/DelegatedProperties.kt
+++ STDEx/src/main/kotlin/org/tobi29/stdex/DelegatedProperties.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 Tobi29
+ * Copyright 2012-2018 Tobi29
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.tobi29.stdex
 
 import org.tobi29.stdex.atomic.AtomicReference
-import kotlin.jvm.Volatile
 import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
