b94a58b6f80e011ba60b50d7b8c7c333ed1220c6
Tobi29 <tobi291019@gmail.com>
2018-04-26 16:38:03 +0200

    Update path for code generation

--- resources/codegen/codegen.sh
+++ resources/codegen/codegen.sh
@@ -14,14 +14,14 @@ CODEGEN_HOME="$(findHome)"
 ENGINE_HOME="$(realpath "$CODEGEN_HOME/../..")"
 
 echo "-- Generating arrays"
-"$CODEGEN_HOME/GenArrays.kts" > "$ENGINE_HOME/Arrays/src/main/kotlin/org/tobi29/arrays/Arrays.kt"
+"$CODEGEN_HOME/GenArrays.kts" > "$ENGINE_HOME/arrays/src/main/kotlin/org/tobi29/arrays/Arrays.kt"
 for arrayType in Boolean Byte Short Int Long Float Double Char; do
-    "$CODEGEN_HOME/GenArrays.kts" "$arrayType" > "$ENGINE_HOME/Arrays/src/main/kotlin/org/tobi29/arrays/${arrayType}Arrays.kt"
+    "$CODEGEN_HOME/GenArrays.kts" "$arrayType" > "$ENGINE_HOME/arrays/src/main/kotlin/org/tobi29/arrays/${arrayType}Arrays.kt"
 done
 
 echo "-- Generating number conversions"
-"$CODEGEN_HOME/GenNumberConversions.kts" > "$ENGINE_HOME/STDEx/src/main/kotlin/org/tobi29/stdex/NumberConversions.kt"
-"$CODEGEN_HOME/GenNumberConversions128.kts" > "$ENGINE_HOME/Utils/src/main/kotlin/org/tobi29/utils/NumberConversions128.kt"
+"$CODEGEN_HOME/GenNumberConversions.kts" > "$ENGINE_HOME/stdex/src/main/kotlin/org/tobi29/stdex/NumberConversions.kt"
+"$CODEGEN_HOME/GenNumberConversions128.kts" > "$ENGINE_HOME/utils/src/main/kotlin/org/tobi29/utils/NumberConversions128.kt"
 
 echo "-- Generating timezone data"
 zoneinfo="/usr/share/zoneinfo"
@@ -35,13 +35,13 @@ regions=(
 )
 
 "$CODEGEN_HOME/GenTzData.kts" $(for zone in "${regions[@]}"; do find "$zoneinfo/$zone" -type f -printf "$zone%P "; done) \
-    > "$ENGINE_HOME/ChronoUtils/src/main/kotlin/org/tobi29/chrono/TzData.kt"
+    > "$ENGINE_HOME/chrono/src/main/kotlin/org/tobi29/chrono/TzData.kt"
 
 echo "-- Generating embedded iana database"
 function ianaDbGen {
     curl "https://www.iana.org/assignments/media-types/$1.csv" \
         | "$CODEGEN_HOME/StripIana.kts" \
-        | "$CODEGEN_HOME/GenStringDataJVM.kts" "com.j256.simplemagik" "iana$2" > "$ENGINE_HOME/SimpleMagikJVM/src/main/kotlin/com/j256/simplemagik/IanaDb$2JVM.kt"
+        | "$CODEGEN_HOME/GenStringDataJVM.kts" "com.j256.simplemagik" "iana$2" > "$ENGINE_HOME/simplemagik-jvm/src/main/kotlin/com/j256/simplemagik/IanaDb$2JVM.kt"
 }
 
 ianaDbGen "application" "Application"
@@ -57,4 +57,4 @@ ianaDbGen "video" "Video"
 echo "-- Generating embedded magic database"
 zcat "$CODEGEN_HOME/magic.gz" \
     | "$CODEGEN_HOME/StripMagic.kts" \
-    | "$CODEGEN_HOME/GenStringDataJVM.kts" "com.j256.simplemagik" "magic" > "$ENGINE_HOME/SimpleMagikJVM/src/main/kotlin/com/j256/simplemagik/MagicDbJVM.kt"
+    | "$CODEGEN_HOME/GenStringDataJVM.kts" "com.j256.simplemagik" "magic" > "$ENGINE_HOME/simplemagik-jvm/src/main/kotlin/com/j256/simplemagik/MagicDbJVM.kt"
