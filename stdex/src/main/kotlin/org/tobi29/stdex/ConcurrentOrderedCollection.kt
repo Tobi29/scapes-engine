7b354fcbde60866bd8ad60bfed19efb086d43936
Git out :V <tobi291019@gmail.com>
2018-07-13 02:31:54 +0200

    Fixes and tweaks to concurrent ordered collection

--- stdex/src/main/kotlin/org/tobi29/stdex/ConcurrentOrderedCollection.kt
+++ stdex/src/main/kotlin/org/tobi29/stdex/ConcurrentOrderedCollection.kt
@@ -19,9 +19,8 @@ package org.tobi29.stdex
 import org.tobi29.stdex.atomic.AtomicLong
 
 class ConcurrentOrderedCollection<T>(
-        private val comparator: Comparator<T> = DummyComparator()
-) : AbstractCollection<T>(),
-        MutableCollection<T> {
+    private val comparator: Comparator<T> = DummyComparator()
+) : AbstractCollection<T>(), MutableCollection<T> {
     private val uidCounter = AtomicLong(Long.MIN_VALUE)
     private val set = ConcurrentSortedSet<Entry>()
 
@@ -44,19 +43,28 @@ class ConcurrentOrderedCollection<T>(
     }
 
     override fun remove(element: T) =
-            removeAll { it == element }
+        set.remove(Entry(element, 0))
 
     override fun removeAll(elements: Collection<T>) =
-            removeAll { it in elements }
+        elements.any { remove(it) }
 
-    override fun retainAll(elements: Collection<T>) =
-            removeAll { it !in elements }
+    override fun retainAll(elements: Collection<T>): Boolean {
+        val iterator = iterator()
+        var modified = false
+        while (iterator.hasNext()) {
+            if (iterator.next() !in elements) {
+                iterator.remove()
+                modified = true
+            }
+        }
+        return modified
+    }
 
-    override fun contains(element: T) =
-            set.any { it == element }
+    override fun contains(element: T): Boolean =
+        set.contains(Entry(element, 0L))
 
     override fun containsAll(elements: Collection<T>) =
-            elements.all { contains(it) }
+        elements.all { contains(it) }
 
     override fun equals(other: Any?): Boolean {
         if (other !is ConcurrentOrderedCollection<*>) {
@@ -72,19 +80,28 @@ class ConcurrentOrderedCollection<T>(
 
     override fun toString() = joinToString(prefix = "[", postfix = "]")
 
-    private inner class Entry(val value: T) : Comparable<Entry> {
-        private val uid = uidCounter.incrementAndGet()
-
+    private inner class Entry(
+        val value: T,
+        private val uid: Long = uidCounter.incrementAndGet()
+    ) : Comparable<Entry> {
         override fun compareTo(other: Entry) =
-                comparator.compare(value, other.value).let {
-                    if (uid > other.uid) 1
-                    else if (uid < other.uid) -1
-                    else 0
-                }
-    }
+            comparator.compare(value, other.value).let {
+                if (uid > other.uid) 1
+                else if (uid < other.uid) -1
+                else 0
+            }
 
-    private class DummyComparator<T> : Comparator<T> {
-        override fun compare(a: T,
-                             b: T) = 0
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other == null || other !is ConcurrentOrderedCollection<*>.Entry)
+                return false
+            return value == other.value
+        }
+
+        override fun hashCode(): Int = value?.hashCode() ?: 0
     }
+}
+
+private class DummyComparator<T> : Comparator<T> {
+    override fun compare(a: T, b: T) = 0
 }
